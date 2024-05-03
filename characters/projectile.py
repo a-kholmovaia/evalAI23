e1aa79dd0cd7b4618a7aca2a687fb9b6acb96ada
Timofei Ushakov <tommccalester@gmail.com>
2024-05-03 11:16:10 +0200

    Implement the Projectile's policy Add simple behaviour for projectiles

--- characters/projectile.py
+++ characters/projectile.py
@@ -1,7 +1,16 @@
 from characters.enemy import Enemy
+from scenes.scene_state import SceneState
 
 class Projectile(Enemy):
     def __init__(self, game_screen, start_position, sprite_master):
         super().__init__(game_screen, start_position, sprite_master)
         self.current_action = "hit"
+
+    def policy(self, scene_state: SceneState) -> str:
+        """
+        Moves left and attacks constantly
+        """
+        self.current_position[0] -= self.speed 
+        return "hit"
+
 
