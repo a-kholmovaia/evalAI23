951cff4442a435eaa061f962847a01e7c41f4901
Timofei Ushakov <tommccalester@gmail.com>
2024-05-04 10:49:56 +0200

    Refactore Projectile and DistantAttackTestScene Add new assets for projectiles Remove instantiation of the Projectile's SpriteMaster in DistantAttackTestScene Add instantiation of the Projectile's SpriteMaster in Projectile

--- characters/projectile.py
+++ characters/projectile.py
@@ -1,9 +1,12 @@
 from characters.enemy import Enemy
 from scenes.scene_state import SceneState
+from masters.sprite_master import SpriteMaster
 
 class Projectile(Enemy):
-    def __init__(self, game_screen, start_position, sprite_master):
-        super().__init__(game_screen, start_position, sprite_master)
+    def __init__(self, game_screen, start_position):
+        super().__init__(game_screen, start_position, SpriteMaster("levels/test_levels/distant_attack/enemy", 
+                              idle=0, walk=0, attack=4, 
+                              hurt=0, death=3, block=0, start_action="hit"))
         self.current_action = "hit"
 
     def policy(self, scene_state: SceneState) -> str:
--- scenes/distant_attack_test_scene.py
+++ scenes/distant_attack_test_scene.py
@@ -1,7 +1,6 @@
 import pygame
 from scenes.scene00 import ScenePrelevel00
 from masters.save_master import SaveMaster
-from masters.sprite_master import SpriteMaster
 from characters.projectile import Projectile
 
 class DistantAttackTestScene(ScenePrelevel00):
@@ -10,6 +9,4 @@ class DistantAttackTestScene(ScenePrelevel00):
                  clock : pygame.time.Clock, font : pygame.font.Font, FPS=60, display_instructions=True):
         super().__init__(scene_path, save_master, game_screen, clock, font)
         self.id = 102
-        self.enemies = [Projectile(game_screen, self.enemy_pos, SpriteMaster("levels/test_levels/distant_attack/enemy", 
-                              idle=0, walk=0, attack=1, 
-                              hurt=0, death=0, block=0, start_action="hit"))]
+        self.enemies = [Projectile(game_screen, self.enemy_pos)]
