99fa8899b41067f4e39d84832a509df8d74c0933
ali <kholmovaia@students.uni-marburg.de>
2024-05-04 15:25:54 +0200

    refactor close attack

--- characters/prelevel_enemy1.py
+++ characters/prelevel_enemy1.py
@@ -14,16 +14,7 @@ class PrelevelEnemy1(Enemy):
         selected_action = ""
          # Randomly decide to attack or stay idle
         if self.cal_distance2player(scene_state.get_player_pos()):
-            if self.current_action == "fight": 
-                    # if the action was fight and the frame index is 0 again (action was executed)
-                    if self.sprite_master.round_done:
-                        selected_action = 'idle'
-                    else: # excetion was not executed, continue
-                        selected_action = 'fight'
-            elif random.random() > 0.97:  # probability to attack
-                selected_action = 'fight'
-            else:
-                selected_action = 'idle'
+            selected_action = self.close_attack()
         elif self.health<=0 and self.death_counter>0:
             selected_action = "death"
             self.death_counter -= self.speed     
@@ -37,4 +28,17 @@ class PrelevelEnemy1(Enemy):
             self.current_position = (-100, -100)
             return "death"
             
+        return selected_action
+
+    def close_attack(self):
+        if self.current_action == "fight": 
+                    # if the action was fight and the frame index is 0 again (action was executed)
+                if self.sprite_master.round_done:
+                    selected_action = 'idle'
+                else: # excetion was not executed, continue
+                    selected_action = 'fight'
+        elif random.random() > 0.97:  # probability to attack
+            selected_action = 'fight'
+        else:
+            selected_action = 'idle'
         return selected_action
