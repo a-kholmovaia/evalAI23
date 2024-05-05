cb314616560db599de74b5f5214cdfbaa44e8e50
Timofei Ushakov <tommccalester@gmail.com>
2024-05-05 14:57:14 +0200

    Fix shooting

--- characters/wizard.py
+++ characters/wizard.py
@@ -42,35 +42,32 @@ class Wizard(Enemy):
             self.current_position = (-100, -100)
             return "death"
 
-        # if self.current_action in ["distant_fight", "shoot"]:
-        #     # If it's one of the hit frames and it hasn't fired yet then return "shoot" instead of "fight"
-        #     if int(self.fight_counter) == len(self.sprite_master.shoot_images) // 2 and not self.has_shot:
-        #         self.fight_counter -= self.speed
-        #         self.has_shot = True
-        #         return "shoot"
-        #     # If the attack animation isn't over yet then return "fight"
-        #     if self.fight_counter > 0:
-        #         self.fight_counter -= self.speed
-        #         return "distant_fight"
-        #     # Otherwise return "idle"
-        #     else:
-        #         self.fight_counter = len(self.sprite_master.attack_images)
-        #         self.elapsed_time = 0
-        #         self.has_shot = False
-        #         print("I'm finished my attack")
-        #         return "idle"
-        # # If the current action is "idle" 
-        # # then update the elapsed time and check if it's bigger than the cooldown time 
-        # else:
-        #     self.elapsed_time += scene_state.get_elapsed_time()
-        #     print(f"elapsed time = {self.elapsed_time}")
-        #     # If the cooldown is over then return "fight" to begin a new attack
-        #     # otherwise keep waiting
-        #     if self.elapsed_time >= self.attack_cooldown:
-        #         return "distant_fight"
-        #     else:
-        #         print("I'm waiting")
-        #         return "idle"
+        if self.current_action in ["distant_fight", "shoot"]:
+            # If it's one of the hit frames and it hasn't fired yet then return "shoot" instead of "fight"
+            if 3.0 <= self.sprite_master.frame_index and not self.has_shot:
+                self.has_shot = True
+                return "shoot"
+            # If the attack animation isn't over yet then return "fight"
+            if not self.sprite_master.round_done:
+                return "distant_fight"
+            # Otherwise return "idle"
+            else:
+                self.elapsed_time = 0
+                self.has_shot = False
+                print("I'm finished my attack")
+                return "idle"
+        # If the current action is "idle" 
+        # then update the elapsed time and check if it's bigger than the cooldown time 
+        else:
+            self.elapsed_time += scene_state.get_elapsed_time()
+            print(f"elapsed time = {self.elapsed_time}")
+            # If the cooldown is over then return "fight" to begin a new attack
+            # otherwise keep waiting
+            if self.elapsed_time >= self.attack_cooldown:
+                return "distant_fight"
+            else:
+                print("I'm waiting")
+                return "idle"
 
         if self.current_action in ["close_fight", "hit"]:
             if self.sprite_master.round_done:
